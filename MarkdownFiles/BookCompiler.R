2e4adf27cf1be47ebe5ea2e436c7a68812c850c0
Jacob Montgomery <jacob.montg@gmail.com>
2017-08-26 18:10:04 -0500

    Update to course book for R Chapter

--- MarkdownFiles/BookCompiler.R
+++ MarkdownFiles/BookCompiler.R
@@ -2,9 +2,9 @@
 # NOTE: there will be a warning about one Rmd file missing a first-level heading, ignore it as it is for the index
 
 library("bookdown")
-#setwd("~/Github/jmontgomery.github.io")
+setwd("~/Github/jmontgomery.github.io")
 setwd("~/Documents/jmontgomery.github.io/MarkdownFiles")
 
 ?render_book
-bookdown::render_book(input="../MarkdownFiles/index.Rmd", output_dir="~/Documents/jmontgomery.github.io/book")
+bookdown::render_book(input="index.Rmd", output_dir="~/Github/jmontgomery.github.io/PS363Coursebook")
 
