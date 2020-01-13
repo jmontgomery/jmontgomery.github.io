3fd7f97a9912b4be77f2414229483f09db38689e
Jacob Montgomery <jacob.montg@gmail.com>
2020-01-13 12:29:03 -0600

    Initial commit for 2020 redesign
    - Altered landing page
    - Started PDS book and organized directory

---
+++ PDS/PDSBookMarkdownFiles/BookCompiler.R
@@ -0,0 +1,8 @@
+# Run this code to compile the book! Make sure you set the correct working directory (should be the same wd with all the Rmd files)
+# NOTE: there will be a warning about one Rmd file missing a first-level heading, ignore it as it is for the index
+
+library("bookdown")
+setwd("~/Github/jmontgomery.github.io/PDS/PDSBookMarkdownFiles")
+
+bookdown::render_book(input="index.Rmd", output_dir="~/Github/jmontgomery.github.io/PDS/PDSBook")
+
