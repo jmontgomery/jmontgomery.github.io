36eb31aab76b9394552d215bcb047cb39318f808
Mariah Yelenick <myelenick@wustl.edu>
2020-01-22 11:12:17 -0600

    writing pds rmd getting started

---
+++ PDS/PDSBookMarkdownFiles/BookCompilerMariah.r
@@ -0,0 +1,8 @@
+# Run this code to compile the book! Make sure you set the correct working directory (should be the same wd with all the Rmd files)
+# NOTE: there will be a warning about one Rmd file missing a first-level heading, ignore it as it is for the index
+
+library("bookdown")
+setwd("C:/Users/maria/OneDrive/WUSTL/2019-20/PDS/jmontgomery.github.io/PDS/PDSBookMarkdownFiles")
+
+bookdown::render_book(input="index.Rmd", output_dir="~/Github/jmontgomery.github.io/PDS/PoliticalDataScience")
+
