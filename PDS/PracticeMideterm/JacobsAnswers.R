a16ecab4f59cc883b0f3305c975da3e2e2254239
Jacob Montgomery <jacob.montg@gmail.com>
2020-02-26 15:47:42 -0600

    slight movement on practice midterm

--- PDS/PracticeMideterm/JacobsAnswers.R
+++ PDS/PracticeMideterm/JacobsAnswers.R
@@ -14,3 +14,6 @@ stateData<-read_csv('http://ippsr.msu.edu/sites/default/files/correlatesofstatep
 table(stateData$state) ## 118 observations per state
 table(stateData$year) ## 51 observations per year
 table(table(stateData$state, stateData$year)) # only one observation for each state/year
+
+
+plot(stateData$undocumented_immigrants~stateData$immig_laws_restrict)
