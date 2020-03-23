1ab1b0c02195cee7dfcbc424f673576438e12940
Jacob Montgomery <jacob.montg@gmail.com>
2020-03-23 09:00:23 -0500

    midterm

---
+++ untitled folder/Trial.R
@@ -0,0 +1,9 @@
+
+library(tidyverse)
+
+Speeches<-read_csv("https://raw.githubusercontent.com/jmontgomery/jmontgomery.github.io/master/PDS/Datasets/State_Union.csv")
+Approval<-read_csv("https://raw.githubusercontent.com/jmontgomery/jmontgomery.github.io/master/PDS/Datasets/Pres_Approval.csv")
+
+
+
+blah$Text[1]
